a1eacc6e0d5b31cc9cefc7d449f31cf34d38417e
Huw William Mawson <32649003+huwmawson@users.noreply.github.com>
2019-03-01 12:28:13 +0100

    Add files via upload

---
+++ IGCSE PRM Number 2 (November 2016 paper 23)/IGCSE PRM Number 2 (November 2016 paper 23)/Module1.vb
@@ -0,0 +1,101 @@
+﻿Module Module1
+
+    Dim candidates(3) As String
+    Dim voteRecord(3) As Integer
+    Dim count As Integer = 0
+    Dim vote As Integer
+
+    Sub getcands()
+        Dim numCands As Integer
+        While numCands < 2 Or numCands > 4
+
+            Console.WriteLine("How many candidates are standing?")
+            numCands = Console.ReadLine()
+        End While
+
+        For i = 0 To numCands - 1
+            Console.WriteLine("What is the name of candidate number " & i + 1)
+            candidates(i) = Console.ReadLine()
+        Next
+
+    End Sub
+
+    Sub getVotes()
+
+
+
+        While count < 30
+
+            vote = 0
+
+            While vote < 1 Or vote > 4
+                Console.WriteLine("PLease Enter your vote: 1: for candidate 1, 2: for candidate 2, 3: for candidate 3, 4: for candidate 4, -1 to stop voting")
+                vote = Console.ReadLine()
+
+
+                Select Case vote
+                Case = 1
+                    voteRecord(0) = voteRecord(0) + 1
+                Case = 2
+                    voteRecord(1) = voteRecord(1) + 1
+                Case = 3
+                    voteRecord(2) = voteRecord(2) + 1
+                Case = 4
+                    voteRecord(3) = voteRecord(3) + 1
+                Case = -1
+                    Exit Sub
+                Case Else
+                    Console.WriteLine("Vote Invalid")
+            End Select
+
+            End While
+
+
+            count = count + 1
+
+
+        End While
+
+
+    End Sub
+    Sub OutputRecords()
+        For i = 0 To 3
+            Console.WriteLine(candidates(i) & "  " & voteRecord(i))
+        Next
+
+    End Sub
+
+    Sub getresults()
+        If voteRecord(0) > voteRecord(1) And voteRecord(0) > voteRecord(2) And voteRecord(0) > voteRecord(3) Then
+            Console.WriteLine(candidates(0) & "is the Winner with " & voteRecord(0) & " votes")
+        ElseIf voteRecord(1) > voteRecord(0) And voteRecord(1) > voteRecord(2) And voteRecord(1) > voteRecord(3) Then
+            Console.WriteLine(candidates(1) & "is the Winner with " & voteRecord(1) & " votes")
+        ElseIf voteRecord(2) > voteRecord(0) And voteRecord(2) > voteRecord(1) And voteRecord(2) > voteRecord(3) Then
+            Console.WriteLine(candidates(2) & "is the Winner with " & voteRecord(2) & " votes")
+        ElseIf voteRecord(3) > voteRecord(0) And voteRecord(3) > voteRecord(1) And voteRecord(3) > voteRecord(2) Then
+            Console.WriteLine(candidates(3) & "is the Winner with " & voteRecord(3) & " votes")
+        Else
+            Console.WriteLine("No Overall Winner")
+        End If
+    End Sub
+
+    Sub Main()
+
+        getcands()
+
+        getVotes()
+
+        OutputRecords()
+
+        getresults()
+
+
+
+
+        Console.ReadKey()
+
+
+
+    End Sub
+
+End Module
---
+++ IGCSE PRM Number 2 (November 2016 paper 23)/IGCSE PRM Number 2 (November 2016 paper 23)/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("IGCSE PRM Number 2 (November 2016 paper 23)")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("IGCSE PRM Number 2 (November 2016 paper 23)")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1b6240ba-7274-4152-9ca1-aee9a42f96fc")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
